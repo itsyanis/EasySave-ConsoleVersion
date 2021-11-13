92fef0a6177191c562efd8d6a3b2bce6768356e5
Yanis MAAFI <yanis.maafi.dz@viacesi.fr>
2021-11-13 19:44:23 +0100

    check

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -8,7 +8,7 @@ namespace EasySave
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World !");
             
             BackupJob newJob = new BackupJob("ma sauvegarde", "@C:\\Users\\ASUS\\Desktop\\file.txt", "@C:\\Users\\Domuments\\file.txt", "Complete");
            
