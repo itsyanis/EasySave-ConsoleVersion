a8803d85b608f98b4f2c50879d1376123a3e4665
Yanis MAAFI <yanis.maafi.dz@viacesi.fr>
2021-11-14 18:30:55 +0100

    add method "writeOnBackupFile"

--- EasySave/BackupJob.cs
+++ EasySave/BackupJob.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json;
 
 namespace EasySave
 {
@@ -20,6 +21,27 @@ namespace EasySave
             this.sourcePath = sourcePath;
             this.destinationPath = destinationPath;
             this.type = type;
+        }
+
+        // This method will create the 'backupFile' if it hasn't been created, and replenish it with the BackupJob informations
+        public void writeOnBackupFile(string path, BackupJob JobInformations)
+        {
+            string JsonInformation =  JsonSerializer.Serialize(JobInformations);    // Convert BackupJob informations to JSON 
+
+            if (File.Exists(path) == true)                                          // Check if 'backupFile' exist
+            {
+                using (StreamWriter file = File.AppendText(path))                   
+                {
+                    file.WriteLine(JsonInformation);                               // If the 'backupFile' exist just append the JSON informations
+                } 
+
+            }else
+            {
+                using (StreamWriter file = File.CreateText(path))                 // If the 'backupFile' doesn't exist we create it and put ON informations
+                {
+                    file.WriteLine(JsonInformation);                    
+                }
+            }
         }
 
     }
