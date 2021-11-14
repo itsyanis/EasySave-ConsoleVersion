8b4fda715fa7c3abf5113d50a1a5e531ab4369c7
Yanis MAAFI <yanis.maafi.dz@viacesi.fr>
2021-11-14 18:31:24 +0100

    a simple test

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -1,5 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
+using System.Text.Json;
+
 
 namespace EasySave
 {
@@ -8,11 +9,39 @@ namespace EasySave
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
-            
-            BackupJob newJob = new BackupJob("ma sauvegarde", "@C:\\Users\\ASUS\\Desktop\\file.txt", "@C:\\Users\\Domuments\\file.txt", "Complete");
-           
-            string json = JsonConvert.SerializeObject(newJob);
+            string backupName = string.Empty;
+            string sourcePath = string.Empty; ;
+            string destinationPath = string.Empty; ;
+            string type = string.Empty;
+            string userChoice = string.Empty;
+
+            Console.WriteLine("EASYSAVE");
+
+            Console.WriteLine(
+                                "1- Create a Backup Job" +
+                                "2- Execute a Backup ");
+
+            userChoice = Console.ReadLine();
+
+            if(userChoice == "1")
+            {
+                Console.WriteLine("Enter your Backup name :");
+                 backupName = Console.ReadLine();
+
+                Console.WriteLine("Enter your source path :");
+                sourcePath = Console.ReadLine();
+
+                Console.WriteLine("Enter your destination path :");
+                destinationPath = Console.ReadLine();
+
+                Console.WriteLine("Enter your your backup type :");
+                type = Console.ReadLine();
+            }
+
+            BackupJob newJob = new BackupJob(backupName, sourcePath, destinationPath, type);
+            newJob.writeOnBackupFile(@"C:\\Users\\ASUS\\Desktop\\test.txt", newJob);
+
+            string json = JsonSerializer.Serialize(newJob);
 
             Console.WriteLine(json);
         }
