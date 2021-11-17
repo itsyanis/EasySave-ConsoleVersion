6996b3cc9650c12e733337df3b3777a6eca26710
Yanis MAAFI <yanis.maafi.dz@viacesi.fr>
2021-11-17 10:04:21 +0100

    Add exception on 'writeOnFile' method

--- EasySave/Data.cs
+++ EasySave/Data.cs
@@ -36,21 +36,29 @@ namespace EasySave
         {
             string JsonInformations = JsonConvert.SerializeObject(Informations);     // Convert DataLog informations to JSON 
 
-            if (File.Exists(path) == true)                                              // Check if 'File' exist
+         try
             {
-                using (StreamWriter logFile = File.AppendText(path))                    // If the 'File' exist just append the JSON informations
+                if (File.Exists(path) == true)                                              // Check if 'File' exist
                 {
-                    logFile.WriteLine(JsonInformations);
+                    using (StreamWriter logFile = File.AppendText(path))                    // If the 'File' exist just append the JSON informations
+                    {
+                        logFile.WriteLine(JsonInformations);
+                    }
                 }
-            }
-            else
-            {
-                using (StreamWriter logFile = File.CreateText(path))                     // If the 'File' doesn't exist we create it and put ON Json informations
+                else
                 {
-                    logFile.WriteLine(JsonInformations);
+                    using (StreamWriter logFile = File.CreateText(path))                     // If the 'File' doesn't exist we create it and put ON Json informations
+                    {
+                        logFile.WriteLine(JsonInformations);
+                    }
                 }
-            }
 
+            }catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Une erreur est survenue ... ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
 }
