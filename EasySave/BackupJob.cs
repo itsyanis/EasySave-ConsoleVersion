9104e4507ed36f10ed50cb311bac2420d42be82d
Yanis MAAFI <yanis.maafi.dz@viacesi.fr>
2021-11-19 11:59:41 +0100

    Show all jobs

--- EasySave/BackupJob.cs
+++ EasySave/BackupJob.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
-using System.Text;
-using System.Text.Json;
 
 namespace EasySave
 {
@@ -12,29 +10,30 @@ namespace EasySave
     class BackupJob : Data
     {
         // Properties, Setters & Getters
-        public string type { get; set; }
+        public string Type { get; private set; }
 
         // Constructor
         public BackupJob() { }
-        public BackupJob(string backupName, string sourcePath, string destinationPath, string type)
+        public BackupJob(string BackupNameEntry, string SourcePathEntry, string DestinationPathEntry, string TypeEntry)
         {
-            this.backupName = backupName;
-            this.sourcePath = sourcePath;
-            this.destinationPath = destinationPath;
-            this.type = type;
+            this.BackupName = BackupNameEntry;
+            this.SourcePath = SourcePathEntry;
+            this.DestinationPath = DestinationPathEntry;
+            this.Type = TypeEntry;
         }
 
         // This method will return a specific backupJob 
-        public string getSpecificJob(string backupName)
+        public string GetSpecificJob(string BackupName)
         {
             string[] All_Lines = File.ReadAllLines(@"C:\EasySave\Backup.txt");    // get all content of backupFile 
+
             foreach (string line in All_Lines)                                   // Loop line by line 
             {
                 var backupJob = (JObject)JsonConvert.DeserializeObject(line);         // Deserialize the each line 
 
                 string name = backupJob["backupName"].Value<string>();               // Extract the backup name from each line
 
-                if (name == backupName)                                             // Compare if the name is the same with the backupName introduced by user 
+                if (name == BackupName)                                             // Compare if the name is the same with the backupName introduced by user 
                 {
                     return line;
                 }
@@ -44,9 +43,29 @@ namespace EasySave
 
 
         // This method will return all backupJob 
-        public string getAllJob()
+        public void ShowAllJobs()
         {
-            return null;
+            string BackupFile = ConfigurationManager.AppSettings.Get("BackupFile");   // Get the Backup file path
+
+            if (new FileInfo(BackupFile).Length != 0)                 // Check if file is not empty
+            {
+                string[] All_Lines = File.ReadAllLines(BackupFile);    // get all content of backupFile 
+
+                foreach (string line in All_Lines)
+                {
+                    var backupJob = (JObject)JsonConvert.DeserializeObject(line);         // Deserialize the each line 
+
+                    Console.WriteLine("*********************************************************************");
+                    Console.WriteLine("- Name : " + backupJob["BackupName"] + "\n- Source Path : " + backupJob["SourcePath"] + "\n- Destination Path : " + backupJob["DestinationPath"]);
+                }
+                Console.WriteLine("*********************************************************************");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No backup job has been created.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
 }
