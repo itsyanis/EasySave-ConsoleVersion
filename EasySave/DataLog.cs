e1e3d8e608e0148141442512beea3510b7a9f72d
Yanis MAAFI <yanis.maafi@viacesi.fr>
2021-12-20 13:26:52 +0100

    Edit "write on LogFile" function

--- EasySave/DataLog.cs
+++ EasySave/DataLog.cs
@@ -9,12 +9,12 @@ namespace EasySave
     {
         // Properties, Setters & Getters
         public long Size { get; private set; }
-        public DateTime Timestamp { get; private set; }
+        public string Timestamp { get; private set; }
         public TimeSpan TrasnferTime { get; private set; }
 
 
         // Constructor
-        public DataLog(string BackupName, string SourcePath, string DestinationPath, DateTime Timestamp, long Size, TimeSpan TrasnferTime)
+        public DataLog(string BackupName, string SourcePath, string DestinationPath, string Timestamp, long Size, TimeSpan TrasnferTime)
         {
             this.BackupName = BackupName;
             this.SourcePath = SourcePath;
